f20450ca061262f2e46cb348d5dfb461c707905f
theodorejohnduran <theodorejohn_duran@yahoo.com>
2020-01-30 12:50:26 +0800

    Add project files.

---
+++ 7Segment.A/Form1.Designer.vb
@@ -0,0 +1,249 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.labelA = New System.Windows.Forms.Label()
+        Me.labelF = New System.Windows.Forms.Label()
+        Me.labelB = New System.Windows.Forms.Label()
+        Me.labelG = New System.Windows.Forms.Label()
+        Me.labelE = New System.Windows.Forms.Label()
+        Me.labelC = New System.Windows.Forms.Label()
+        Me.labelD = New System.Windows.Forms.Label()
+        Me.btnOne = New System.Windows.Forms.Button()
+        Me.btnTwo = New System.Windows.Forms.Button()
+        Me.btnThree = New System.Windows.Forms.Button()
+        Me.btnFour = New System.Windows.Forms.Button()
+        Me.btnFive = New System.Windows.Forms.Button()
+        Me.btnSix = New System.Windows.Forms.Button()
+        Me.btnSeven = New System.Windows.Forms.Button()
+        Me.btnEight = New System.Windows.Forms.Button()
+        Me.btnNine = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Black
+        Me.PictureBox1.Location = New System.Drawing.Point(290, 60)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(480, 425)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'labelA
+        '
+        Me.labelA.BackColor = System.Drawing.Color.Red
+        Me.labelA.Location = New System.Drawing.Point(454, 78)
+        Me.labelA.Name = "labelA"
+        Me.labelA.Size = New System.Drawing.Size(150, 30)
+        Me.labelA.TabIndex = 1
+        '
+        'labelF
+        '
+        Me.labelF.BackColor = System.Drawing.Color.Red
+        Me.labelF.Location = New System.Drawing.Point(418, 106)
+        Me.labelF.Name = "labelF"
+        Me.labelF.Size = New System.Drawing.Size(30, 150)
+        Me.labelF.TabIndex = 2
+        '
+        'labelB
+        '
+        Me.labelB.BackColor = System.Drawing.Color.Red
+        Me.labelB.Location = New System.Drawing.Point(608, 108)
+        Me.labelB.Name = "labelB"
+        Me.labelB.Size = New System.Drawing.Size(30, 150)
+        Me.labelB.TabIndex = 3
+        '
+        'labelG
+        '
+        Me.labelG.BackColor = System.Drawing.Color.Red
+        Me.labelG.Location = New System.Drawing.Point(452, 256)
+        Me.labelG.Name = "labelG"
+        Me.labelG.Size = New System.Drawing.Size(150, 30)
+        Me.labelG.TabIndex = 4
+        '
+        'labelE
+        '
+        Me.labelE.BackColor = System.Drawing.Color.Red
+        Me.labelE.Location = New System.Drawing.Point(418, 285)
+        Me.labelE.Name = "labelE"
+        Me.labelE.Size = New System.Drawing.Size(30, 150)
+        Me.labelE.TabIndex = 5
+        '
+        'labelC
+        '
+        Me.labelC.BackColor = System.Drawing.Color.Red
+        Me.labelC.Location = New System.Drawing.Point(608, 285)
+        Me.labelC.Name = "labelC"
+        Me.labelC.Size = New System.Drawing.Size(30, 150)
+        Me.labelC.TabIndex = 6
+        '
+        'labelD
+        '
+        Me.labelD.BackColor = System.Drawing.Color.Red
+        Me.labelD.Location = New System.Drawing.Point(450, 437)
+        Me.labelD.Name = "labelD"
+        Me.labelD.Size = New System.Drawing.Size(150, 30)
+        Me.labelD.TabIndex = 7
+        '
+        'btnOne
+        '
+        Me.btnOne.Location = New System.Drawing.Point(43, 60)
+        Me.btnOne.Name = "btnOne"
+        Me.btnOne.Size = New System.Drawing.Size(75, 23)
+        Me.btnOne.TabIndex = 8
+        Me.btnOne.Text = "1"
+        Me.btnOne.UseVisualStyleBackColor = True
+        '
+        'btnTwo
+        '
+        Me.btnTwo.Location = New System.Drawing.Point(43, 89)
+        Me.btnTwo.Name = "btnTwo"
+        Me.btnTwo.Size = New System.Drawing.Size(75, 23)
+        Me.btnTwo.TabIndex = 9
+        Me.btnTwo.Text = "2"
+        Me.btnTwo.UseVisualStyleBackColor = True
+        '
+        'btnThree
+        '
+        Me.btnThree.Location = New System.Drawing.Point(43, 118)
+        Me.btnThree.Name = "btnThree"
+        Me.btnThree.Size = New System.Drawing.Size(75, 23)
+        Me.btnThree.TabIndex = 10
+        Me.btnThree.Text = "3"
+        Me.btnThree.UseVisualStyleBackColor = True
+        '
+        'btnFour
+        '
+        Me.btnFour.Location = New System.Drawing.Point(43, 147)
+        Me.btnFour.Name = "btnFour"
+        Me.btnFour.Size = New System.Drawing.Size(75, 23)
+        Me.btnFour.TabIndex = 11
+        Me.btnFour.Text = "4"
+        Me.btnFour.UseVisualStyleBackColor = True
+        '
+        'btnFive
+        '
+        Me.btnFive.Location = New System.Drawing.Point(43, 176)
+        Me.btnFive.Name = "btnFive"
+        Me.btnFive.Size = New System.Drawing.Size(75, 23)
+        Me.btnFive.TabIndex = 12
+        Me.btnFive.Text = "5"
+        Me.btnFive.UseVisualStyleBackColor = True
+        '
+        'btnSix
+        '
+        Me.btnSix.Location = New System.Drawing.Point(43, 205)
+        Me.btnSix.Name = "btnSix"
+        Me.btnSix.Size = New System.Drawing.Size(75, 23)
+        Me.btnSix.TabIndex = 13
+        Me.btnSix.Text = "6"
+        Me.btnSix.UseVisualStyleBackColor = True
+        '
+        'btnSeven
+        '
+        Me.btnSeven.Location = New System.Drawing.Point(43, 235)
+        Me.btnSeven.Name = "btnSeven"
+        Me.btnSeven.Size = New System.Drawing.Size(75, 23)
+        Me.btnSeven.TabIndex = 14
+        Me.btnSeven.Text = "7"
+        Me.btnSeven.UseVisualStyleBackColor = True
+        '
+        'btnEight
+        '
+        Me.btnEight.Location = New System.Drawing.Point(43, 264)
+        Me.btnEight.Name = "btnEight"
+        Me.btnEight.Size = New System.Drawing.Size(75, 23)
+        Me.btnEight.TabIndex = 15
+        Me.btnEight.Text = "8"
+        Me.btnEight.UseVisualStyleBackColor = True
+        '
+        'btnNine
+        '
+        Me.btnNine.Location = New System.Drawing.Point(43, 293)
+        Me.btnNine.Name = "btnNine"
+        Me.btnNine.Size = New System.Drawing.Size(75, 23)
+        Me.btnNine.TabIndex = 16
+        Me.btnNine.Text = "9"
+        Me.btnNine.UseVisualStyleBackColor = True
+        '
+        'btnReset
+        '
+        Me.btnReset.Location = New System.Drawing.Point(43, 462)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(75, 23)
+        Me.btnReset.TabIndex = 17
+        Me.btnReset.Text = "Reset"
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(838, 567)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnNine)
+        Me.Controls.Add(Me.btnEight)
+        Me.Controls.Add(Me.btnSeven)
+        Me.Controls.Add(Me.btnSix)
+        Me.Controls.Add(Me.btnFive)
+        Me.Controls.Add(Me.btnFour)
+        Me.Controls.Add(Me.btnThree)
+        Me.Controls.Add(Me.btnTwo)
+        Me.Controls.Add(Me.btnOne)
+        Me.Controls.Add(Me.labelD)
+        Me.Controls.Add(Me.labelC)
+        Me.Controls.Add(Me.labelE)
+        Me.Controls.Add(Me.labelG)
+        Me.Controls.Add(Me.labelB)
+        Me.Controls.Add(Me.labelF)
+        Me.Controls.Add(Me.labelA)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents labelA As Label
+    Friend WithEvents labelF As Label
+    Friend WithEvents labelB As Label
+    Friend WithEvents labelG As Label
+    Friend WithEvents labelE As Label
+    Friend WithEvents labelC As Label
+    Friend WithEvents labelD As Label
+    Friend WithEvents btnOne As Button
+    Friend WithEvents btnTwo As Button
+    Friend WithEvents btnThree As Button
+    Friend WithEvents btnFour As Button
+    Friend WithEvents btnFive As Button
+    Friend WithEvents btnSix As Button
+    Friend WithEvents btnSeven As Button
+    Friend WithEvents btnEight As Button
+    Friend WithEvents btnNine As Button
+    Friend WithEvents btnReset As Button
+End Class
---
+++ 7Segment.A/Form1.vb
@@ -0,0 +1,117 @@
+﻿Public Class Form1
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles labelF.Click
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles labelA.Click
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles labelB.Click
+
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        labelA.Visible = True
+        labelB.Visible = True
+        labelC.Visible = True
+        labelD.Visible = True
+        labelE.Visible = True
+        labelF.Visible = True
+        labelG.Visible = True
+    End Sub
+
+    Private Sub btnOne_Click(sender As Object, e As EventArgs) Handles btnOne.Click
+        labelA.Visible = False
+        labelB.Visible = True
+        labelC.Visible = True
+        labelD.Visible = False
+        labelE.Visible = False
+        labelF.Visible = False
+        labelG.Visible = False
+    End Sub
+
+    Private Sub btnTwo_Click(sender As Object, e As EventArgs) Handles btnTwo.Click
+        labelA.Visible = True
+        labelB.Visible = True
+        labelC.Visible = False
+        labelD.Visible = True
+        labelE.Visible = True
+        labelF.Visible = False
+        labelG.Visible = True
+    End Sub
+
+    Private Sub btnThree_Click(sender As Object, e As EventArgs) Handles btnThree.Click
+        labelA.Visible = True
+        labelB.Visible = True
+        labelC.Visible = True
+        labelD.Visible = True
+        labelE.Visible = False
+        labelF.Visible = False
+        labelG.Visible = True
+    End Sub
+
+    Private Sub btnFour_Click(sender As Object, e As EventArgs) Handles btnFour.Click
+        labelA.Visible = False
+        labelB.Visible = True
+        labelC.Visible = True
+        labelD.Visible = False
+        labelE.Visible = False
+        labelF.Visible = True
+        labelG.Visible = True
+    End Sub
+
+    Private Sub btnFive_Click(sender As Object, e As EventArgs) Handles btnFive.Click
+        labelA.Visible = True
+        labelB.Visible = False
+        labelC.Visible = True
+        labelD.Visible = True
+        labelE.Visible = False
+        labelF.Visible = True
+        labelG.Visible = True
+    End Sub
+
+    Private Sub btnSix_Click(sender As Object, e As EventArgs) Handles btnSix.Click
+        labelA.Visible = True
+        labelB.Visible = False
+        labelC.Visible = True
+        labelD.Visible = True
+        labelE.Visible = True
+        labelF.Visible = True
+        labelG.Visible = True
+    End Sub
+
+    Private Sub btnSeven_Click(sender As Object, e As EventArgs) Handles btnSeven.Click
+        labelA.Visible = True
+        labelB.Visible = True
+        labelC.Visible = True
+        labelD.Visible = False
+        labelE.Visible = False
+        labelF.Visible = False
+        labelG.Visible = False
+    End Sub
+
+    Private Sub btnEight_Click(sender As Object, e As EventArgs) Handles btnEight.Click
+        labelA.Visible = True
+        labelB.Visible = True
+        labelC.Visible = True
+        labelD.Visible = True
+        labelE.Visible = True
+        labelF.Visible = True
+        labelG.Visible = True
+    End Sub
+
+    Private Sub btnNine_Click(sender As Object, e As EventArgs) Handles btnNine.Click
+        labelA.Visible = True
+        labelB.Visible = True
+        labelC.Visible = True
+        labelD.Visible = True
+        labelE.Visible = False
+        labelF.Visible = True
+        labelG.Visible = True
+    End Sub
+End Class
